1839005f66d629c84c66ca5592a31971acb00b51
consuatin <mjculprint@gmail.com>
2020-12-09 16:09:49 +0100

    removed absolute position

--- game.js
+++ game.js
@@ -1,3 +1,17 @@
+let choices = ['rock', 'paper', 'scissors'];
+
+// Computer choice
+let computerC = function() {
+    var comp = Math.random() * 3 | 0;
+    return (choices[comp]);
+    console.log(choices[comp])
+}
+
+// Player choice
+let playerC = function() {
+    
+}
+
 
 
 
